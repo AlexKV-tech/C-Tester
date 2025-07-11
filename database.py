63c031d9173bc74477ac8b063c648425fa0dd24a
AlexKV-tech <alex_18204@outlook.com>
2025-07-11 21:10:38 +0200

    db files

--- database.py
+++ database.py
@@ -1,2 +1,12 @@
 # During deployment, this will be replaced by a real database.
 TEST_DB = {}
+import sqlalchemy
+import sqlalchemy.ext.declarative as declarative
+import sqlalchemy.orm as orm
+
+
+DB_URL = "postgresql+psycopg2://postgres@localhost/ctests_db"
+engine = sqlalchemy.create_engine(DB_URL)
+SessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative.declarative_base()
+
---
+++ models.py
@@ -0,0 +1,12 @@
+import datetime 
+import sqlalchemy
+import database
+
+class CTest(database.Base):
+    __tablename__ = "c_tests"
+    test_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+    ctest_text = sqlalchemy.Column(sqlalchemy.String, index=True)
+    original_text = sqlalchemy.Column(sqlalchemy.String, index=True)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow)
+    expires_at  = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow)
+
